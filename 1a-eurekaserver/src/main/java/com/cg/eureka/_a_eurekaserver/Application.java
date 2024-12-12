ae81c840b012420a94583a9a5fbdebaa635a5645
Gawade <vaibhavi-vishnu.gawade@capgemini.com>
2024-12-12 16:39:33 +0530

    first commit

---
+++ 1a-eurekaserver/src/main/java/com/cg/eureka/_a_eurekaserver/Application.java
@@ -0,0 +1,16 @@
+package com.cg.eureka._a_eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
